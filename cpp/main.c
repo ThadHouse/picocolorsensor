836c4c903e68a6022a743b47588c0c8f7609c1af
Thad House <thadhouse1@gmail.com>
2022-02-12 22:20:12 -0800

    Make code reliable

--- cpp/main.c
+++ cpp/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include "pico/stdlib.h"
 #include "hardware/uart.h"
@@ -35,6 +36,19 @@ bool read_i2c_data(i2c_inst_t *i2c, uint8_t reg, uint8_t* buffer, uint8_t readLe
     return result == readLen;
 }
 
+void init_device(i2c_inst_t* i2c, uint8_t* i2cBuffer) {
+    i2cBuffer[0] = MAIN_CTRL_REGISTER;
+    i2cBuffer[1] = MAIN_CTRL_RGB_MODE | MAIN_CTRL_LIGHT_SENSOR_ENABLE | MAIN_CTRL_PROXIMITY_SENSOR_ENABLE;
+    i2c_write_timeout_us(i2c, SENSOR_ADDRESS, i2cBuffer, 2, false, 25000);
+
+    i2cBuffer[0] = PROXIMITY_SENSOR_RATE_REGISTER;
+    i2cBuffer[1] = PROXIMITY_SENSOR_RATE_100MS | PROXIMITY_SENSOR_RES_11BIT;
+    i2c_write_timeout_us(i2c, SENSOR_ADDRESS, i2cBuffer, 2, false, 25000);
+
+    i2cBuffer[0] = PROXIMITY_SENSOR_PULSES_REGISTER;
+    i2cBuffer[1] = PROXIMITY_SENSOR_PULSES;
+    i2c_write_timeout_us(i2c, SENSOR_ADDRESS, i2cBuffer, 2, false, 25000);
+}
 
 int main() {
     // Init uart
@@ -68,85 +82,54 @@ int main() {
 
     char outputBuffer[512];
 
-    bool has0 = false;
-    bool has1 = false;
+    unsigned int values[10];
+    uint8_t i2cBuffer[20];
+    bool currentValid0 = false;
+    bool currentValid1 = false;
+    absolute_time_t loopTime;
+    bool reset;
 
     sleep_ms(100);
 
-    // Attempt to connect to i2c0
-    uint8_t i2cBuffer[20];
-    bool readValid = read_i2c_data(i2c0, PART_ID_REGISTER, i2cBuffer, 1);
-    if (readValid) {
-        has0 = i2cBuffer[0] == EXPECTED_PART_ID;
-    }
+    init_device(i2c0, i2cBuffer);
+    read_i2c_data(i2c0, MAIN_STATUS_REGISTER, i2cBuffer, 1);
 
-    if (has0) {
-        i2cBuffer[0] = MAIN_CTRL_REGISTER;
-        i2cBuffer[1] = MAIN_CTRL_RGB_MODE | MAIN_CTRL_LIGHT_SENSOR_ENABLE | MAIN_CTRL_PROXIMITY_SENSOR_ENABLE;
-        i2c_write_timeout_us(i2c0, SENSOR_ADDRESS, i2cBuffer, 2, false, 25000);
+    init_device(i2c1, i2cBuffer);
+    read_i2c_data(i2c1, MAIN_STATUS_REGISTER, i2cBuffer, 1);
 
-        i2cBuffer[0] = PROXIMITY_SENSOR_RATE_REGISTER;
-        i2cBuffer[1] = PROXIMITY_SENSOR_RATE_100MS | PROXIMITY_SENSOR_RES_11BIT;
-        i2c_write_timeout_us(i2c0, SENSOR_ADDRESS, i2cBuffer, 2, false, 25000);
-
-        i2cBuffer[0] = PROXIMITY_SENSOR_PULSES_REGISTER;
-        i2cBuffer[1] = PROXIMITY_SENSOR_PULSES;
-        i2c_write_timeout_us(i2c0, SENSOR_ADDRESS, i2cBuffer, 2, false, 25000);
-
-        read_i2c_data(i2c0, MAIN_STATUS_REGISTER, i2cBuffer, 1);
-    }
-
-    // Attempt to connect to i2c1
-    readValid = read_i2c_data(i2c1, PART_ID_REGISTER, i2cBuffer, 1);
-    if (readValid) {
-        has1 = i2cBuffer[0] == EXPECTED_PART_ID;
-    }
-
-    if (has1) {
-        i2cBuffer[0] = MAIN_CTRL_REGISTER;
-        i2cBuffer[1] = MAIN_CTRL_RGB_MODE | MAIN_CTRL_LIGHT_SENSOR_ENABLE | MAIN_CTRL_PROXIMITY_SENSOR_ENABLE;
-        i2c_write_timeout_us(i2c1, SENSOR_ADDRESS, i2cBuffer, 2, false, 25000);
-
-        i2cBuffer[0] = PROXIMITY_SENSOR_RATE_REGISTER;
-        i2cBuffer[1] = PROXIMITY_SENSOR_RATE_100MS | PROXIMITY_SENSOR_RES_11BIT;
-        i2c_write_timeout_us(i2c1, SENSOR_ADDRESS, i2cBuffer, 2, false, 25000);
-
-        i2cBuffer[0] = PROXIMITY_SENSOR_PULSES_REGISTER;
-        i2cBuffer[1] = PROXIMITY_SENSOR_PULSES;
-        i2c_write_timeout_us(i2c1, SENSOR_ADDRESS, i2cBuffer, 2, false, 25000);
-
-        read_i2c_data(i2c1, MAIN_STATUS_REGISTER, i2cBuffer, 1);
-    }
-
-    unsigned int values[10];
+    memset(values, 0, sizeof(values));
 
     while (1) {
-        absolute_time_t loopTime = make_timeout_time_ms(100);
+        loopTime = make_timeout_time_ms(100);
 
-        if (has0) {
-            readValid = read_i2c_data(i2c0, PROXIMITY_DATA_REGISTER, i2cBuffer, 14);
-            if (readValid) {
-                values[4] = ((i2cBuffer[0] & 0xFF) | ((i2cBuffer[1] & 0xFF) << 8)) & 0x7FF;
-                values[3] = ((i2cBuffer[2] & 0xFF) | ((i2cBuffer[3] & 0xFF) << 8) | ((i2cBuffer[4] & 0xFF) << 16)) & 0x03FFFF;
-                values[2] = ((i2cBuffer[5] & 0xFF) | ((i2cBuffer[6] & 0xFF) << 8) | ((i2cBuffer[7] & 0xFF) << 16)) & 0x03FFFF;
-                values[1] = ((i2cBuffer[8] & 0xFF) | ((i2cBuffer[9] & 0xFF) << 8) | ((i2cBuffer[10] & 0xFF) << 16)) & 0x03FFFF;
-                values[0] = ((i2cBuffer[11] & 0xFF) | ((i2cBuffer[12] & 0xFF) << 8) | ((i2cBuffer[13] & 0xFF) << 16)) & 0x03FFFF;
+        currentValid0 = read_i2c_data(i2c0, MAIN_STATUS_REGISTER, i2cBuffer, 15);
+        if (currentValid0) {
+            if ((i2cBuffer[0] & 0x20) != 0) {
+                init_device(i2c0, i2cBuffer);
+            } else {
+                values[4] = ((i2cBuffer[1] & 0xFF) | ((i2cBuffer[2] & 0xFF) << 8)) & 0x7FF;
+                values[3] = ((i2cBuffer[3] & 0xFF) | ((i2cBuffer[4] & 0xFF) << 8) | ((i2cBuffer[5] & 0xFF) << 16)) & 0x03FFFF;
+                values[2] = ((i2cBuffer[6] & 0xFF) | ((i2cBuffer[7] & 0xFF) << 8) | ((i2cBuffer[8] & 0xFF) << 16)) & 0x03FFFF;
+                values[1] = ((i2cBuffer[9] & 0xFF) | ((i2cBuffer[10] & 0xFF) << 8) | ((i2cBuffer[11] & 0xFF) << 16)) & 0x03FFFF;
+                values[0] = ((i2cBuffer[12] & 0xFF) | ((i2cBuffer[13] & 0xFF) << 8) | ((i2cBuffer[14] & 0xFF) << 16)) & 0x03FFFF;
             }
         }
 
-        if (has1) {
-            readValid = read_i2c_data(i2c1, PROXIMITY_DATA_REGISTER, i2cBuffer, 14);
-            if (readValid) {
-                values[9] = ((i2cBuffer[0] & 0xFF) | ((i2cBuffer[1] & 0xFF) << 8)) & 0x7FF;
-                values[8] = ((i2cBuffer[2] & 0xFF) | ((i2cBuffer[3] & 0xFF) << 8) | ((i2cBuffer[4] & 0xFF) << 16)) & 0x03FFFF;
-                values[7] = ((i2cBuffer[5] & 0xFF) | ((i2cBuffer[6] & 0xFF) << 8) | ((i2cBuffer[7] & 0xFF) << 16)) & 0x03FFFF;
-                values[6] = ((i2cBuffer[8] & 0xFF) | ((i2cBuffer[9] & 0xFF) << 8) | ((i2cBuffer[10] & 0xFF) << 16)) & 0x03FFFF;
-                values[5] = ((i2cBuffer[11] & 0xFF) | ((i2cBuffer[12] & 0xFF) << 8) | ((i2cBuffer[13] & 0xFF) << 16)) & 0x03FFFF;
+        currentValid1 = read_i2c_data(i2c1, MAIN_STATUS_REGISTER, i2cBuffer, 15);
+        if (currentValid1) {
+            if ((i2cBuffer[0] & 0x20) != 0) {
+                init_device(i2c1, i2cBuffer);
+            } else {
+                values[9] = ((i2cBuffer[1] & 0xFF) | ((i2cBuffer[2] & 0xFF) << 8)) & 0x7FF;
+                values[8] = ((i2cBuffer[3] & 0xFF) | ((i2cBuffer[4] & 0xFF) << 8) | ((i2cBuffer[5] & 0xFF) << 16)) & 0x03FFFF;
+                values[7] = ((i2cBuffer[6] & 0xFF) | ((i2cBuffer[7] & 0xFF) << 8) | ((i2cBuffer[8] & 0xFF) << 16)) & 0x03FFFF;
+                values[6] = ((i2cBuffer[9] & 0xFF) | ((i2cBuffer[10] & 0xFF) << 8) | ((i2cBuffer[11] & 0xFF) << 16)) & 0x03FFFF;
+                values[5] = ((i2cBuffer[12] & 0xFF) | ((i2cBuffer[13] & 0xFF) << 8) | ((i2cBuffer[14] & 0xFF) << 16)) & 0x03FFFF;
             }
         }
 
-        snprintf(outputBuffer, sizeof(outputBuffer), "%u,%u,%u,%u,%u,%u,%u,%u,%u,%u\n",
-            values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9]);
+        snprintf(outputBuffer, sizeof(outputBuffer), "%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u\n",
+            currentValid0, currentValid1, values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9]);
 
         uart_puts(uart0, outputBuffer);
 
